07283ae7770917bcc7547e673ce0ac9caeecb82d
silent <silentrc@163.com>
2025-01-19 12:46:35 +0800

    add network ip api

---
+++ ipv6/model.go
@@ -0,0 +1,27 @@
+package ipv6
+
+import "os"
+
+type Dat struct {
+	Data    []byte
+	Version uint16
+	F       *os.File
+	Index   Index
+	Offset  uint32
+}
+
+type Index struct {
+	Start  uint64
+	End    uint64
+	Offlen uint64
+	Count  uint64
+	Data   [][]uint64
+}
+
+type Result struct {
+	IP      string `json:"ip"`
+	Number  uint64 `json:"number"`
+	Country string `json:"country"`
+	Area    string `json:"area"`
+	Offset  uint64 `json:"offset"`
+}
